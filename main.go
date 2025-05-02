50d5a7b01d76dec75b671f349a2018aac1496b99
milemik <milemik68@gmail.com>
2025-05-02 23:01:56 +0200

    Created WorkDay struct
    * Updated initial endpoint to return hardcoded list of work days
    * created function to update response headers -> to have json response

--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
+// Lets start simple. Lets create struct for work log
+// This structure should have: date, hours worked and description
+type WorkDay struct {
+	WorkDate    time.Time `json:"work_date"`
+	HourWorked  int       `json:"hours_worked"`
+	Description string    `json:"description"`
+}
+
+
+func updateResponseHeaders(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+}
+
 func main() {
+	// Lets add some hard cored data to be accessed by our API
+	workDays := []WorkDay{
+		WorkDay{WorkDate: time.Date(2025, time.Month(5), 2, 0, 0, 0, 0, time.UTC), Description: "Second work day", HourWorked: 10},
+		WorkDay{WorkDate: time.Date(2025, time.Month(5), 1, 0, 0, 0, 0, time.UTC), Description: "First work day", HourWorked: 9},
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello world")
+		updateResponseHeaders(w, r)
+		data, err := json.Marshal(workDays)
+		if err != nil {
+			log.Fatal(err)
+		}
+		io.Writer.Write(w, data)
 	})
 
 	log.Println("Starting server on port 8080")
