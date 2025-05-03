be5273a8b87cb4c59c4a046544f3c79e0e716299
milemik <milemik68@gmail.com>
2025-05-03 07:24:44 +0200

    Created initial request for adding work day
    * Created new endpoint /api/work/add
    * Created HttpMessage struct for returning simple message to user

--- main.go
+++ main.go
@@ -17,6 +17,11 @@ type WorkDay struct {
 }
 
 
+type HttpMessage struct {
+	Message string `json:"message"`
+}
+
+
 func updateResponseHeaders(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 }
@@ -27,14 +32,40 @@ func main() {
 		WorkDay{WorkDate: time.Date(2025, time.Month(5), 2, 0, 0, 0, 0, time.UTC), Description: "Second work day", HourWorked: 10},
 		WorkDay{WorkDate: time.Date(2025, time.Month(5), 1, 0, 0, 0, 0, time.UTC), Description: "First work day", HourWorked: 9},
 	}
+	
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/api/work/list/", func(w http.ResponseWriter, r *http.Request) {
 		updateResponseHeaders(w, r)
 		data, err := json.Marshal(workDays)
 		if err != nil {
 			log.Fatal(err)
 		}
 		io.Writer.Write(w, data)
+	})
+
+
+	http.HandleFunc("/api/work/add/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			message, err := json.Marshal(HttpMessage{Message: "method not allowed"})
+			if err != nil {
+				log.Fatal(err)
+			}
+			io.Writer.Write(w, message)
+		}
+		requestBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer r.Body.Close()
+		var workDay WorkDay
+		// get data from request
+		err = json.Unmarshal(requestBody, &workDay)
+		if err != nil {
+			log.Fatal(err)
+		}
+		workDays = append(workDays, workDay)
+		updateResponseHeaders(w, r)
+		io.Writer.Write(w, requestBody)
 	})
 
 	log.Println("Starting server on port 8080")
